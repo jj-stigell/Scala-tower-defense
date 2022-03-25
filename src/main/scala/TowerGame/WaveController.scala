d1e591723155c5aab3c3aac7ffc23e59c7c03d97
jj-stigell <jj.stigell@gmail.com>
2022-03-26 00:09:23 +0900

    controlling waves

---
+++ src/main/scala/TowerGame/WaveController.scala
@@ -0,0 +1,35 @@
+package TowerGame
+
+object WaveController {
+
+  val maxWaves = Settings.maxWaves
+  var currentWave = 0
+
+  // New Wave
+
+  def launchNewWave() = {
+
+    if (currentWave <= maxWaves) {
+
+      currentWave += 1
+      Updater.resetArea()
+      Updater.updateStats()
+
+      Game.startButton.enabled = false
+      Game.roundOver = false
+
+    } else {
+      //viimeinen rundi pelattu
+    }
+
+  }
+
+
+
+
+
+
+
+
+
+}
