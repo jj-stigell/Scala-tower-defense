fcb2a0a9b6f67a9aaa4249d62aaa68479e7ca7b9
jj-stigell <jj.stigell@gmail.com>
2022-03-25 14:39:15 +0900

    Update settings, add datatypes for clarity

--- src/main/scala/TowerGame/Settings.scala
+++ src/main/scala/TowerGame/Settings.scala
@@ -1,27 +1,32 @@
 package TowerGame
 
+/**
+ * Settings for the game, do not edit any other part of the game code except these
+ * Mind the correct datatypes
+ */
 object Settings {
 
-  val width = 1200
-  val height = 800
-  val fullHeight = height + 40    // take into account the control panel at the bottom
-  val towerPrice = 10
-  val maxHealth = 100
-  val hpLossPerEnemy = 1
-  val coolDownTime = 100
-  val coolDownPerCycle = 1
-  val enemyHealth = 10
-  val enemySpeed: Double = 7.0              // scalar for the enemy speed
-  val maxWaves = 10
-  val title = "TowerDefense"
-  val resizable = false
+  val width: Int = 1200
+  val height: Int = 800
+  val fullHeight: Int = height + 40       // take into account the control panel at the bottom
+  val towerPrice: Int = 10
+  val rewardFromKill: Int = 5
+  val maxHealth: Int = 100
+  val hpLossPerEnemy: Int = 1
+  val coolDownTime: Int = 100
+  val coolDownPerCycle: Int = 1
+  val enemyHealth: Int = 10
+  val enemySpeed: Double = 7.0            // scalar for the enemy speed
+  val maxWaves: Int = 10
+  val title: String = "TowerDefense"
+  val resizable: Boolean = false
 
-  // Each row must be same lenght
+  // Each row must be same length
   // 1 road, roads cannot be side to side
   // 2 entry point, must be on the side, only one
   // 3 out point, must be on the side, only one
   // Default map
-  var map = Array(
+  var map: Array[Array[Int]] = Array(
     Array(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
     Array(2, 1, 1, 1, 0, 1, 1, 1, 0, 0, 0, 0, 0),
     Array(0, 0, 0, 1, 0, 1, 0, 1, 0, 0, 0, 0, 0),
@@ -33,18 +38,23 @@ object Settings {
     Array(0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0)
   )
 
-  // how many blocks the map has, horizontal and vertical
-  val totalHorizontalBlocks = this.map(0).length
-  val totalVerticalBlocks = this.map.length
-  val blockLengthX = this.width / this.totalHorizontalBlocks
-  val blockLengthY = this.height / this.totalVerticalBlocks
+  // how many blocks the map has, horizontal and vertical and thei lengths
+  var totalHorizontalBlocks: Int = this.map(0).length
+  var totalVerticalBlocks: Int = this.map.length
+  var blockLengthX: Int = this.width / this.totalHorizontalBlocks
+  var blockLengthY: Int = this.height / this.totalVerticalBlocks
 
+  /**
+   * Set new map for the game and update blocks and lengths
+   *
+   * @param newMap New map Array[Array[Int]]
+   */
   def setMap(newMap: Array[Array[Int]] ) = {
     this.map = newMap
-    val totalHorizontalBlocks = this.map(0).length
-    val totalVerticalBlocks = this.map.length
-    val blockLengthX = this.width / totalHorizontalBlocks
-    val blockLengthY = this.height / totalVerticalBlocks
+    totalHorizontalBlocks = this.map(0).length
+    totalVerticalBlocks = this.map.length
+    blockLengthX = this.width / totalHorizontalBlocks
+    blockLengthY = this.height / totalVerticalBlocks
   }
 
 }
