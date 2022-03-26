9073344536918e7dcbd74f9e2e2751b0b36661b6
jj-stigell <jj.stigell@gmail.com>
2022-03-26 15:41:56 +0900

    simplified enemy class by combining initial values to path and direction values

--- src/main/scala/TowerGame/Area.scala
+++ src/main/scala/TowerGame/Area.scala
@@ -17,40 +17,23 @@ object Area {
 
   private val towerSize: Int = (((Settings.width / Settings.totalHorizontalBlocks) + (Settings.height / Settings.totalVerticalBlocks)) / 3)
 
-  // Initialize the starting location of enemies and the direction
+  // Initialize the starting location of enemies and the direction and calculate path and directions on the map
   val initLoc = PathFinder.enemyInitialLocation()
   val initDir = PathFinder.findInitialDirection(initLoc)
+  val path = (PathFinder.enemyPath(initLoc, initDir)).map(_._1)
+  val directions = (PathFinder.enemyPath(initLoc, initDir)).map(_._2)
 
-  val pathAndDirections = (PathFinder.enemyPath(initLoc, initDir)).unzip
-
-  val path = pathAndDirections._1
-  val directions = pathAndDirections._2
-
+  // Correct values based on the gui dimensions
   val correctedInitlDir = Vector2D(initDir._1 * Settings.enemySpeed, initDir._2 * Settings.enemySpeed)
   val correctedInitlLoc = Vector2D(Settings.blockLengthX * (initLoc._1 + (-1 * initDir._1)), Settings.blockLengthY* (initLoc._2 + (-1 * initDir._2)))
-
-
-
-
-  // Vector multiplied with the speed scalar
-
   val correctedDirections = directions.map( x => Vector2D(x._1 * Settings.enemySpeed, x._2 * Settings.enemySpeed))
   val correctedPath = path.map( x => Vector2D(x._1 * Settings.blockLengthX.toDouble, x._2 * Settings.blockLengthY.toDouble))
-
-  //val correctedDirections: Buffer[(Double, Double)] = directions.map(_._1 * Settings.enemySpeed).zip(directions.map(_._2 * Settings.enemySpeed))
-  //val correctedPath: Buffer[(Double, Double)] = path.map(_._1 * Settings.blockLengthX.toDouble).zip(path.map(_._2 * Settings.blockLengthY.toDouble))
-
-
-
-
-  // add the initial enemy
-  enemies += new Enemy(correctedInitlDir, correctedInitlLoc, correctedPath, correctedDirections)
 
 
   // When space steps one time unit forward, all enemies move a step forward
   def step() = {
     if (numberOfEnemies > 0 && tick % Settings.correctedInterval == 0) {
-      enemies += new Enemy(correctedInitlDir, correctedInitlLoc, correctedPath, correctedDirections)
+      enemies += new Enemy(Buffer(correctedInitlLoc) ++ correctedPath, correctedDirections)
       numberOfEnemies -= 1
     }
     tick += 1
@@ -68,28 +51,19 @@ object Area {
 
   }
 
-  def resetEnemyBuffer() = enemies = Buffer[Enemy](new Enemy(correctedInitlDir, correctedInitlLoc, correctedPath, correctedDirections))
+
+  def resetEnemyBuffer() = enemies = Buffer[Enemy]()
+
 
   def checkBlocking() = {
     if (towers.exists(tower => (tower.place.x - towerLocation.x).abs < towerSize && (tower.place.y - towerLocation.y).abs < towerSize)) Game.blocked = true else Game.blocked = false
 
-
     println(correctedInitlLoc)
     println(correctedPath)
-
-
-
   }
 
 
-
-
-
-
-
   def newTowerLocation(locationMouse: MouseMoved) = towerLocation = Vector2D(locationMouse.point.x - Settings.xCorrection, locationMouse.point.y - Settings.yCorrection)
-
-
 
 
     /*
